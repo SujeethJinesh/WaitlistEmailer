89cf4fa2100131eb3849f284b09c2df8142eca35
SujeethJinesh <sujeethjinesh@gmail.com>
2018-11-02 15:47:40 -0400

    Add Email

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import schedule
-import time
 import smtplib
+
+import time
+
+from credentials import LOGIN, PASSWORD
 
 email_sent = False
 iterations = 0
@@ -12,22 +14,21 @@ def check_waitlist():
         print("checking_waitlist")
 
 
-def send_email(from_addr="sujeethjinesh@gmail.com", to_addr_list="sujeethjinesh@gmail.com",
-               subject="4510 Spot Opened Up", message="Sign up yooo",
-               login, password,
+def send_email(subject="4510 Spot Opened Up", message="Sign up yooo",
                smtpserver='smtp.gmail.com:587'):
     global email_sent
-    header = 'From: %s\n' % from_addr
-    header += 'To: %s\n' % ','.join(to_addr_list)
+    header = 'From: %s\n' % LOGIN
+    header += 'To: %s\n' % ','.join([LOGIN])
     header += 'Subject: %s\n\n' % subject
     message = header + message
 
     server = smtplib.SMTP(smtpserver)
     server.starttls()
-    server.login(login, password)
-    problems = server.sendmail(from_addr, to_addr_list, message)
+    server.login(LOGIN, PASSWORD)
+    problems = server.sendmail(LOGIN, [LOGIN], message)
+    if problems:
+        print(problems)
     server.quit()
-
     email_sent = True
 
 
